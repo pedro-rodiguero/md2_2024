be556535ddf1d12c92c6047c5392875b59d7042e
pemiinem <190139323@aluno.unb.br>
2024-11-13 13:15:08 -0300

    chore: add addition_table.go file for organization purposes

---
+++ addition_table.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func additionTable(n int) {
+	fmt.Printf("Tabela Z_%d para adição modular:\n", n)
+	for i := 0; i < n; i++ {
+		for j := 0; j < n; j++ {
+			fmt.Printf("%d ", (i+j)%n)
+		}
+		fmt.Println()
+	}
+}
--- main.go
+++ main.go
@@ -66,16 +66,6 @@ func main() {
 	}
 }
 
-func additionTable(n int) {
-	fmt.Printf("Tabela Z_%d para adição modular:\n", n)
-	for i := 0; i < n; i++ {
-		for j := 0; j < n; j++ {
-			fmt.Printf("%d ", (i+j)%n)
-		}
-		fmt.Println()
-	}
-}
-
 func multiplicationTable(n int) {
 	fmt.Printf("Tabela Z_%d para multiplicação modular:\n", n)
 	for i := 0; i < n; i++ {
