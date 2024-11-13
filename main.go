2feb83163153275a95e63e690f0c7fe1967b3bfc
pemiinem <190139323@aluno.unb.br>
2024-11-13 13:15:42 -0300

    chore: add multiplication_table.go file for organization purposes

--- main.go
+++ main.go
@@ -65,13 +65,3 @@ func main() {
 		}
 	}
 }
-
-func multiplicationTable(n int) {
-	fmt.Printf("Tabela Z_%d para multiplicação modular:\n", n)
-	for i := 0; i < n; i++ {
-		for j := 0; j < n; j++ {
-			fmt.Printf("%d ", (i*j)%n)
-		}
-		fmt.Println()
-	}
-}
---
+++ multiplication_table.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func multiplicationTable(n int) {
+	fmt.Printf("Tabela Z_%d para multiplicação modular:\n", n)
+	for i := 0; i < n; i++ {
+		for j := 0; j < n; j++ {
+			fmt.Printf("%d ", (i*j)%n)
+		}
+		fmt.Println()
+	}
+}
